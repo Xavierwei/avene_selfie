230d1f4f3b0eeb41fc4fcf97bd7220ef5290a109
tonysh518 <tony@fuel-it-up.com>
2014-03-12 02:47:13 +0800

    加入videojs

--- js/lp.base.js
+++ js/lp.base.js
@@ -195,7 +195,6 @@ LP.use(['jquery', 'api', 'easing', 'fileupload', 'flash-detect', 'swfupload', 's
             // reset 
             transforms = [];
             dragHelper.setTransform( $('.imgwrap-opts') , "inherit" );
-            console.clear();
             
             imgWidth = img.width || $(img).width();
             imgHeight =  img.height || $(img).height();
@@ -741,11 +740,12 @@ LP.use(['jquery', 'api', 'easing', 'fileupload', 'flash-detect', 'swfupload', 's
     });
 
 
-    LP.action('uploadImg', function(data){
-        console.log(data);
+    LP.action('preview', function(){
+		var data = dragHelper.getResult();
+		console.log(data);
 
         api.ajax('preview' , data , function( result ){
-            console.log(result);
+            //console.log(result);
             gotoStep(5);
             //uploadComplete();
         });
